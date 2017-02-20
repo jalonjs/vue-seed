d975ad2ee667841d4e4ff8d05ce4990f1ec992bb
wangzhiliang <wangzhiliang@didichuxing.com>
2017-02-20 18:19:16 +0800

    增加 指令

--- src/components/Action.vue
+++ src/components/Action.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="input-box">
-    <input type="text" v-model.trim="text" placeholder="输入item" @keyup="keyup" v-focus/>
+    <input type="text" v-focus v-model.trim="text" placeholder="输入item" @keyup="keyup" v-focus/>
     <button @click="add" style="margin-left: 10px;">{{ btnText }}</button>
   </div>
 </template>
